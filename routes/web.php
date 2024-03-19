2f01687c376201412d8991d55352aede0f957e86
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-19 17:59:24 +0100

    added route for ProjectController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,6 +20,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', GuestHomeController::class)->name('guest.home');
 
 Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
+
+Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+Route::get('/projects/{project', [ProjectController::class, 'show'])->name('projects.show');
+Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+Route::get('projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+Route::put('projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+Route::delete('projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
