0f35a82e688f4a614fd8ddce1c4bb1b0498af268
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-20 00:44:21 +0100

    filled file show

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,17 +2,25 @@
 
 @section('title', 'Projects')
 
+@section('cdns')
+{{--Fontawesome--}}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" 
+crossorigin="anonymous" referrerpolicy="no-referrer" />
+@endsection
+
 
 @section('content')
 <h1>Projects</h1>
 <table class="table table-dark">
     <thead>
         <tr>
-          <th scope="col">#</th>
+          <th scope="col">Id</th>
           <th scope="col">Titolo</th>
           <th scope="col">Slug</th>
           <th scope="col">Creato il</th>
           <th scope="col">Ultima modifica</th>
+          <th scope="col"></th>
         </tr>
       </thead>
       <tbody class="table-group-divider">
@@ -22,9 +30,24 @@
             <td>{{$project->title}}</td>
             <td>{{$project->slug}}</td>
             <td>{{$project->created_at}}</td>
-            <td>{{$project->Updated_at}}</td>
+            <td>{{$project->updated_at}}</td>
             <td>
-                <a href=""></a>
+                <div class="d-flex justify-content-end gap-2">
+                    <a href="{{route('admin.projects.show',$project)}}" class="btn btn-primary">
+                        <i class="fa-solid fa-eye"></i>
+                    </a>
+                    <a href="{{route('admin.projects.edit',$project)}}" class="btn btn-warning">
+                        <i class="fa-solid fa-pencil"></i>
+                    </a>
+
+                <form action="{{route('admin.projects.destroy',$project->id)}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button  class="btn btn-danger">
+                        <i class="fa-solid fa-trash-can"></i>
+                    </button>
+                </form>
+                </div>
             </td>
         </tr>
         @empty
