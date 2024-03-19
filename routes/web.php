0cdf0bc6bf55823faa424a36655cfebfdc0a3086
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-19 17:35:07 +0100

    added model project with migration factory and controller

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [GuestHomeController::class])->name('guest.home');
+Route::get('/', GuestHomeController::class)->name('guest.home');
 
-Route::get('/admin', [AdminHomeController::class])->middleware(['auth', 'verified'])->name('admin.home');
+Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
