05d96079e4d0f4f220a3f72b0b36c8dd18b88cc3
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-19 18:16:41 +0100

    added projects crud

--- routes/web.php
+++ routes/web.php
@@ -19,17 +19,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', GuestHomeController::class)->name('guest.home');
 
-Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
-
-Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
-Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
-Route::get('/projects/{project', [ProjectController::class, 'show'])->name('projects.show');
-Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
-Route::get('projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
-Route::put('projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
-Route::delete('projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
 
 
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
+    Route::get('', AdminHomeController::class)->name('home');
+    Route::resource('projects', ProjectController::class);
+});
+
+//Rotte protette dell'amministratore
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
