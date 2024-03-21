de3cb4e0b69c0a4ad851f96c055876dfd8d20419
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-03-21 19:03:57 +0100

    added UpdateProjectRequest

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -10,25 +10,25 @@
             <div class="col-12">
                 <div class="mb-3">
                   <label for="title" class="form-label">Title</label>
-                  <input type="text" class="form-control" id="title" name="title" value="{{old('title','')}}">
+                  <input type="text" class="form-control" id="title" name="title" value="{{old('title',$project->title)}}">
                 </div>
             </div>
             <div class="col-11">
                 <div class="mb-3">
                     <label for="image" class="form-label">Image</label>
-                    <input type="url" class="form-control" id="image" name="image" value="{{old('image','')}}">
+                    <input type="url" class="form-control" id="image" name="image" value="{{old('image',$project->image)}}">
                 </div>
             </div>
             <div class="col-1">
                 <div class="mb-3">
-                   <img src="{{old('image', 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM=')}}" 
+                   <img src="{{old('thumb',$project->image) ? old('thumb',$project->image) : 'https://media.istockphoto.com/id/1147544807/vector/thumbnail-image-vector-graphic.jpg?s=612x612&w=0&k=20&c=rnCKVbdxqkjlcs3xH87-9gocETqpspHFXu5dIGB4wuM='}}" 
                    alt="#" class="img-fluid" id="preview">
                 </div>
             </div>
             <div class="col-12">
                 <div class="form-floating mb-3">
                     <label for="content" class="form-label"></label>
-                    <textarea class="form-control" id="content" rows="30" name="content">{{old('content','')}}</textarea>
+                    <textarea class="form-control" id="content" rows="30" name="content">{{old('content',$project->content)}}</textarea>
                 </div>
             </div>
             <div class="col-12 d-flex justify-content-between">
